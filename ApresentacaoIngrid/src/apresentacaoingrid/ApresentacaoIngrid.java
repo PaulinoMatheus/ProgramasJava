a9856b20ca56241f62f11fe597fbd6d315957eaf
Matheus Lucena <matheuspaulino2004@gmail.com>
2025-10-30 23:36:09 -0300

    Adicionando Merge Sort - Kailaine

--- ApresentacaoIngrid/src/apresentacaoingrid/ApresentacaoIngrid.java
+++ ApresentacaoIngrid/src/apresentacaoingrid/ApresentacaoIngrid.java
@@ -19,6 +19,8 @@ public class ApresentacaoIngrid {
 
         int[] listaInsertion = listaBaguncada.clone();// clona a lista bagunçada
         int[] listaBubble = listaBaguncada.clone();//Matheus L: clona a lista bagunçada para calcular o método bubble de ordenação
+        int[] listaMerge = listaBaguncada.clone();// Lista para o Merge Sort - Kailaine
+
         int[] lista = listaBaguncada.clone();
         int[] lista3 = listaBaguncada.clone();
 
@@ -38,7 +40,14 @@ public class ApresentacaoIngrid {
         long inicioBubble = System.nanoTime(); // Matheus L: Utilizado para registrar o momento de início do algoritmo BubbleSort
         int comparacoesBubble = bubbleSort(listaBubble); // Matheus L: Utiliza a variável de quantidade de comparacoes para saber quando tempo levou até estar concluida
         long fimBubble = System.nanoTime(); // Matheus L: Registra o momento que a ordenação BubbleSort termina
-        long tempoExecucaoBubble = fimBubble - inicioBubble; // Matheus L: Subtrai os valores de início e fim para obter o tempo exato de duração do processo de ordenação4
+        long tempoExecucaoBubble = fimBubble - inicioBubble; // Matheus L: Subtrai os valores de início e fim para obter o tempo exato de duração do processo de ordenação
+
+        // Merge Sort - Kailaine
+        long inicioMerge = System.nanoTime();
+        int comparacoesMerge = mergeSort(listaMerge, 0, listaMerge.length - 1 );
+        long fimMerge = System.nanoTime();
+        long tempoMerge = fimMerge - inicioMerge;
+
 
         System.out.println("============== RESULTADOS =============");
         System.out.println("Lista Baguncada: ");
@@ -47,6 +56,9 @@ public class ApresentacaoIngrid {
         System.out.println(Arrays.toString(listaInsertion));
         System.out.println("Lista Ordenada (BubbleSort): "); // Matheus L: Transforma a lista já ordenada com o BubbleSort em texto para exibir para o usuário
         System.out.println(Arrays.toString(listaBubble));
+        System.out.println("Lista ordenada (MergeSort):");// Lista Ordenada
+        System.out.println(Arrays.toString(listaMerge));
+
 
         System.out.println("========================================");
         System.out.println("RESULTADOS DAS BUSCAS: ");
@@ -66,12 +78,18 @@ public class ApresentacaoIngrid {
         System.out.printf("InsertionSort | %d  | %.3f %n", tempoExecucaoInsertion, tempoExecucaoInsertion / 1_000_000.0);//%d é um numero, %.3 é um numero float com 3 casas decimais 
         System.out.printf("LinearSearch | %d  | %.3f %n", tempoExecucaoLinear, tempoExecucaoLinear / 1_000_000.0);//%d é um numero, %.3 é um numero float com 3 casas decimais 
         System.out.printf("BubbleSort | %d | %.3f %n", tempoExecucaoBubble, tempoExecucaoBubble/1_000_000.0);//Matheus L: %d é um número, %3 é um número float com 3 casas decimais
+        // Tempo de execução
+        System.out.printf("Merge Sort | %d | %.3f %n", tempoMerge, tempoMerge / 1_000_000.0);
+
         
         System.out.println("=========================================");
         System.out.println("algoritmo | comparacoes");
         System.out.printf("InsertionSort | %d %n", comparacoesInsertion);
         System.out.printf("BubbleSort | %d %n", comparacoesBubble);
         System.out.printf("LinearSearch | %d %n", comparacoesLinear); //Matheus L: Indica o algoritmo e a quantidade de iterações que precisaram ser feitas para concluir a ordenação
+        // Tabela de comparações
+        System.out.printf("Merge Sort | %d %n", comparacoesMerge);
+
     }
 
     public static int insertionSort(int[] lista) {
@@ -125,4 +143,72 @@ public class ApresentacaoIngrid {
 
 
     }
+    // Merge Sort - Kailaine
+public static int mergeSort (int[] array, int inicio, int fim){
+    int comparacoes = 0;
+
+    // Caso base: se o inicio for maior ou igual que o fim, significa que a lista tem 0 ou 1 elemento (já ordenada)
+    if (inicio < fim) {
+        // Pega o meio, ele irá ser o pivô da divisão.
+        int meio = (inicio + fim) / 2;
+
+        // Divide a lista no meio (soma e armazena as comparações feitas)
+        comparacoes += mergeSort(array, inicio, meio);
+        comparacoes += mergeSort(array,meio + 1, fim);
+
+        // Depois de divida, junta as duas metades (soma e armazena as comparações feitas)
+        comparacoes += merge(array, inicio,meio, fim);
+    }
+    // Retorna as comparações feitas no algoritmo
+    return comparacoes;
+}
+
+public static int merge(int[] array, int inicio, int meio, int fim){
+    int comparacoes = 0;
+    // Define o tamanho das metades
+    int tamanhoEsq = meio - inicio + 1;
+    int tamanhoDir = fim - meio;
+
+    // Define os arrays das metades
+    int [] arrayEsq = new int[tamanhoEsq];
+    int [] arrayDir = new int[tamanhoDir];
+
+    // Popula o array esquerdo com os valores correspondente do lado esquerdo da lista
+    for (int i = 0; i< tamanhoEsq;++i){
+        arrayEsq[i] = array[inicio + i];
+    }
+    // Popula o array direito com os valores correspondente do lado direito da lista
+    for (int j = 0; j < tamanhoDir; ++j){
+        arrayDir[j] = array[meio + 1 + j];
+    }
+    int i = 0, j = 0;
+    int k = inicio;
+    // Enquanto houver elementos em ambas as metades, compara e copia o menor (incrementa a variavel comparacoes)
+    while (i < tamanhoEsq && j < tamanhoDir) {
+        comparacoes++;
+
+        if (arrayEsq[i] <= arrayDir[j]){
+            array[k] = arrayEsq[i];
+            i++;
+        } else {
+            array[k]= arrayDir[j];
+            j++;
+        }
+        k++;
+    }
+    // copia os elementos restantes (não incrementa comparacoes por que não vericamos os elementos dentro dos loops)
+    while (i < tamanhoEsq){
+        array[k]= arrayEsq[i];
+        i++;
+        k++;
+}
+    while (j < tamanhoDir){
+        array[k]= arrayDir[j];
+        j++;
+        k++;
+    }
+    return comparacoes;
+}
+
+
 }
